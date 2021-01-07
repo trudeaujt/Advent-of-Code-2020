de87c34223f06223aff38b60c6aca42743a59adc
Jonathan <trudeaujt@gmail.com>
2021-01-08 01:15:46 +0900

    Update day07.js

--- day07/day07.js
+++ day07/day07.js
@@ -1,40 +1,15 @@
 export const parseInput = (input) => input.split('\n');
 
-var graph = {
-
-}
-
-// class Node {
-//     constructor(value) {
-//       this.value = value;
-//       this.adjacents = []; // adjacency list
-//     }
-  
-//     addAdjacent(node) {
-//       this.adjacents.push(node);
-//     }
-  
-//     removeAdjacent(node) {
-//       const index = this.adjacents.indexOf(node);
-//       if(index > -1) {
-//         this.adjacents.splice(index, 1);
-//         return node;
-//       }
-//     }
-  
-//     getAdjacents() {
-//       return this.adjacents;
-//     }
-  
-//     isAdjacent(node) {
-//       return this.adjacents.indexOf(node) > -1;
-//     }
-//   }
-
 export const problem1 = (input) => {
-    input.forEach(bag => console.log(bag));
-
-    const [outerBag, innerBags] = 
+ 
+    var bagGraph = input.reduce((graph, entry) => {
+        var [bag, contains] = entry
+            .replace(/bags/g,'bag')
+            .replace('.','')
+            .split(' contain ');
+        contains = contains.split(', ');
+        debugger;
+    }, {});
 
 }
 

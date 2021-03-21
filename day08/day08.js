0292a7627b831ee8639462d05293b4725f323dae
Jonathan Trudeau <trudeaujt@gmail.com>
2021-03-21 19:27:22 +0900

    Update for new IntelliJ workflow

--- day08/day08.js
+++ day08/day08.js
@@ -40,46 +40,7 @@ export const problem1 = (input) => {
 }
 
 export const problem2 = (input) => {
-    let accumulator = 0;
-    let loopDetected = false;
-    let i = 0;
 
-    while(true) {
-        let cur = input[i];
 
-        if(cur.visited === true) {
-            loopDetected = true;
-        } else if(cur.op === ops.NOP) {
-            ++i;
-        } else if(cur.op === ops.ACC) {
-            accumulator += cur.val * cur.sign;
-            ++i;
-        } else if(cur.op === ops.JMP) {
-            i += cur.val * cur.sign;
-        }
-        cur.visited = true;
 
-        if(loopDetected) {
-            loopDetected = false;
-            var haveFlippedThisIteration = false;
-            input.forEach(entry => {
-                entry.visited = false;
-                if(haveFlippedThisIteration === false) {
-                    if(entry.op === ops.NOP && entry.flipped === false) {
-                        debugger;
-                        entry.op = ops.JMP;
-                        entry.flipped = true;
-                        haveFlippedThisIteration = true;
-                    } else if (entry.op === ops.JMP && entry.flipped === false) {
-                        debugger;
-                        entry.op = ops.NOP;
-                        entry.flipped = true;
-                        haveFlippedThisIteration = true;
-                    }
-                }
-            });
-
-        }
-    }
-    return accumulator;
 }

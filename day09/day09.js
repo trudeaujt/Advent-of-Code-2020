7d87145c6767c291751ce2800553384abf8a1eb0
Jonathan Trudeau <trudeaujt@gmail.com>
2021-03-24 17:20:48 +0900

    Day 9 part 1 completed

--- day09/day09.js
+++ day09/day09.js
@@ -1,7 +1,46 @@
 // @flow
 
-export const problem1 = (input) => {
+function removeElement(array, number) {
+    const index = array.indexOf(number);
+    if(index > -1) {
+        array.splice(index, 1);
+        return array;
+    } else {
+        throw new Error(`Element ${number} not found.`)
+    }
+}
 
+export const problem1 = (inputString, preamble) => {
+    const input = inputString.split("\n")
+        .map(elem => parseInt(elem));
+    let slice = input.slice(0, preamble);
+    slice.sort();
+    //Oldest element, used to move the sliding slice window
+    let sliceTail = 0;
+
+    for(let sliceHead = preamble; sliceHead < input.length; ++sliceHead) {
+        let leftIndex = 0, rightIndex = slice.length - 1;
+        let nextValue = input[sliceHead];
+
+        while(leftIndex !== rightIndex) {
+            let sum = slice[leftIndex] + slice[rightIndex];
+            if(sum === nextValue) {
+                break;
+            }
+            else if(sum > nextValue)
+                --rightIndex;
+            else if(sum < nextValue)
+                ++leftIndex;
+        }
+        if(leftIndex === rightIndex)
+            return nextValue;
+
+        const oldestElement = input[sliceTail];
+        removeElement(slice, oldestElement);
+        ++sliceTail;
+        slice.push(nextValue);
+        slice.sort((a, b) => a - b);
+    }
 }
 
 export const problem2 = (input) => {
--- day09/day09.test.js
+++ day09/day09.test.js
@@ -1,2 +1,15 @@
-import { problem1, problem2 } from './day09'
-import { knownInput, unknownInput } from './input'
+import { problem1, problem2 } from './day09.js'
+import { knownInput, unknownInput } from './input.js'
+import assert from "assert";
+
+const PART1_KNOWN_RESULT = 127;
+const PART1_UNKNOWN_RESULT = 373803594;
+
+describe('Day 9 - Part 1', () => {
+    it('should find the number that is not a sum of the previous five numbers for a known input', () => {
+        assert.strictEqual(problem1(knownInput, 5), PART1_KNOWN_RESULT);
+    });
+    it('should find the number that is not a sum of the previous twenty five numbers for an unknown input', function () {
+        assert.strictEqual(problem1(unknownInput, 25), PART1_UNKNOWN_RESULT);
+    });
+})
